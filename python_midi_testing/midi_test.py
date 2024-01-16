6e8f0ab2bf3cda3ee0d067506e8b681ce0310fc7
dmay29 <david70may@gmail.com>
2024-01-16 14:43:56 -0500

    Made quick python code for reading and playing notes from piano

---
+++ python_midi_testing/midi_test.py
@@ -0,0 +1,86 @@
+import mido
+from mido import MidiFile, MidiTrack, Message
+from mido.backends.rtmidi import Input, Output
+from fluidsynth import Synth
+import time
+
+SOUND_PATH = 'python_midi_testing/[GD] Clean Concert Grand.sf2'
+PIANO_NAME = 'Impact GX61 MIDI1'
+# OUTPUT = 'FluidSynth virtual port (77349)'
+
+
+class MidiInterface():
+
+     # FluidSynth setup
+    soundfont_path:str = None
+    fs: Synth = None
+
+    input_port:Input = None
+    output_port:Output = None
+    
+
+    def __init__(self, soundfont_path = SOUND_PATH, piano_name = PIANO_NAME):
+        self.fs = Synth()
+        self.fs.start(driver='coreaudio')  # use coreaudio driver
+        self.sfid = self.fs.sfload(soundfont_path)
+        self.fs.program_select(0, self.sfid, 0, 0)
+        self.soundfont_path = soundfont_path
+        self.input_port:Input = mido.open_input(piano_name)
+        self.output_port:Output = mido.open_output()
+
+    def run(self):
+        try:
+            while True:
+                for msg in self.input_port.iter_pending():
+                    if msg.type == 'note_on':
+                        # Play the note with FluidSynth
+                        self.fs.noteon(0, msg.note, 127)
+
+                    elif msg.type == 'note_off':
+                        # Stop the note with FluidSynth
+                        self.fs.noteoff(0, msg.note)
+                    msg: Message
+                    # Parse MIDI message
+                    if hasattr(msg, 'note'):
+                        note_name, octave = note_number_to_name(msg.note)
+                        print(f'{msg.type}: {note_name}{octave}, Velocity: {msg.velocity}')
+                    else:
+                        print(f'{msg.type}, {msg}')
+
+                    # Pass the MIDI message to the output port
+                    self.output_port.send(msg)
+
+                    
+                
+
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.input_port.close()
+            self.output_port.close()
+
+
+    
+
+
+# Function to convert note number to frequency
+def note_number_to_frequency(note_number):
+    return 440.0 * (2.0 ** ((note_number - 69) / 12.0))
+
+# Function to convert note number to note name
+def note_number_to_name(note_number):
+    notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+    octave = note_number // 12 - 1
+    note_name = notes[note_number % 12]
+    return note_name, octave
+
+if __name__ == '__main__':
+    midi = MidiInterface()
+    midi.run()
+
+
+
+
+
+
+
