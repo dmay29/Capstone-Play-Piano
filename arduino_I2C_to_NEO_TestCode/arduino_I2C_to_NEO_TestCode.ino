2e2493a86c2f6cfd4b5ccf2a2e96ed33ba4bbe82
DavidMese <david.mese07@gmail.com>
2024-01-20 03:11:08 -0500

    Initial Arduino GIGA script. Takes I2C from RPI. Needs work for LED output. Not tested. Comment clarifications. More skeleton.

--- arduino_I2C_to_NEO_TestCode/arduino_I2C_to_NEO_TestCode.ino
+++ arduino_I2C_to_NEO_TestCode/arduino_I2C_to_NEO_TestCode.ino
@@ -5,27 +5,27 @@
   TO UPLOAD AND PROCESS SONG:
     - Send MIDI data over I2C
         MESSAGE FORM: 
-            1) Begin by sending a string "Start PreloadXX" over I2C
+            1) Begin by sending a string "Start PreloadX" over I2C
             2) Then send a string line of 62 values, representing keys 0-61 and the timestamp in the form:
                 - each key is represented by either a 0 or 1 to show if the key is turned on
                 - each value is seperated by a single space
                 - the timestamp is the final value
-                - after each line/timestamp, end the line with 'XX'
-              EXAMPLE: "Start PreloadXX[61 zeroes or ones seperated by a space]*space*[timestamp]XX...
-                                      [61 zeroes or ones seperated by a space]*space*[timestamp1]XX...
-                                      [61 zeroes or ones seperated by a space]*space*[last-timestamp]XX
-                        End PreloadXX"
-            3) End song transfer by sending string "End PreloadXX" over I2C
+                - after each line/timestamp, end the line with 'X'
+              EXAMPLE: "Start PreloadX[61 zeroes or ones seperated by a space]*space*[timestamp]X...
+                                      [61 zeroes or ones seperated by a space]*space*[timestamp1]X...
+                                      [61 zeroes or ones seperated by a space]*space*[last-timestamp]X
+                        End PreloadX"
+            3) End song transfer by sending string "End PreloadX" over I2C
             4) Arduino will save and process the data to create a cascading note effect
 
   TO BEGIN LEARNING/PLAY-ALONG SONG:
-    - Send a string "Start SongXX [time-scale * 10]XX" over I2C
-      1) For example a song at regular speed would require: "Start SongXX 10XX"
-      2) Half-speed would require: "Start SongXX 5XX"
-      3) Double-speed would require: "Start SongXX 20XX"
+    - Send a string "Start SongX [time-scale * 10]X" over I2C
+      1) For example a song at regular speed would require: "Start SongX 10X"
+      2) Half-speed would require: "Start SongX 5X"
+      3) Double-speed would require: "Start SongX 20X"
 
   TO BEGIN Freeplay:
-    - Send a string "Free PlayXX" over I2C
+    - Send a string "Free PlayX" over I2C
 */
 
 #include <Wire.h>
@@ -50,20 +50,22 @@
 #define Octave_4 D3
 #define Octave_5_plus1 D6
 
+//create definitions for MATRIX values for each midi key, and their tails
+#define Pixels_Per_Octave 84
+#define Pixels_Per_Octave_plus1 91
+//some matrix conversion for notes to pixel and the tail
+
+//GlobalVariables
 const int Max_Timestamps = 6000; // Example maximum number of timestamps
 int8_t Song_Data[Max_Timestamps][61]; // 63 by Max_Timestamp data structure [index, binary for Notes 0-61, timestamp]
 uint16_t Song_Data_Timestamp[Max_Timestamps];
 
-//create definitions for MATRIX values for each midi key, and their tails
-
 //convert the recieved notes to a timestamped array of all notes and their tails (have var for tail duration) (have a time scale option)
-
-//create function to cycle through row, wait for timestamp, and execute
-
-//create a recieve event for beginning the song, this should also send the time scale 
 
 int row_Index = 0;
 bool is_Preloading = false;
+bool expecting_Speed = false;
+int song_speed = 0;
 String I2C_Buffer = "";
 
 void setup() {
@@ -80,7 +82,7 @@ void loop() {
 void receiveEvent(int howMany) {
     while (Wire.available()) {
         char c = Wire.read();
-        if (c == 'XX') {
+        if (c == 'X') {
             processBuffer();
             I2C_Buffer = ""; // Clear buffer for next line
         } else {
@@ -90,28 +92,33 @@ void receiveEvent(int howMany) {
 }
 
 void processBuffer() {
-    if (I2C_Buffer == "Start PreloadXX") {
+    if (I2C_Buffer == "Start PreloadX") {
         is_Preloading = true;
         row_Index = 0;
     } 
-    else if (I2C_Buffer == "End PreloadXX") {
+    else if (I2C_Buffer == "End PreloadX") {
         is_Preloading = false;
         //song is stored
     } 
     else if (is_Preloading) {
         parseAndStoreData(I2C_Buffer);
     }
-    else if (I2C_Buffer == "Start SongXX") {
-        startLearning();
-    }    
-    else if (I2C_Buffer == "Free PlayXX") {
+    else if (I2C_Buffer == "Start SongX") {
+        expecting_Speed = true;  // Set the flag to true
+    }   
+    else if (expectingNumber) {
+        song_speed = I2C_Buffer.substring(0, I2C_Buffer.length() - 1).toInt(); // Parse the integer
+        startLearning(song_speed);  // Call the function with the integer
+        expectingNumber = false;  // Reset the flag
+    }
+    else if (I2C_Buffer == "Free PlayX") {
         freePlayMode();
     }
 }
 
 void parseAndStoreData(String data) {
-    if (data.endsWith("XX")) {
-        data.remove(data.length() - 2); // Remove "XX"
+    if (data.endsWith("X")) {
+        data.remove(data.length() - 1); // Remove "X"
 
         int spaceIndex;
         int column = 0; 
@@ -136,6 +143,10 @@ void parseAndStoreData(String data) {
     }
 }
 
+void createTails(){
+
+}
+
 void printFirstXRows(int x) {
     for (int i = 0; i < x; i++) { // Loop through the first 50 rows
         for (int j = 0; j < 61; j++) { // Loop through each column in the row
@@ -146,10 +157,12 @@ void printFirstXRows(int x) {
     }
 }
 
-void startLearning(){
-
+//function to cycle through rows, wait for timestamp, and execute
+void startLearning(int song_speed){
+  
 }
 
+//function for freeplay mode
 void freePlayMode(){
 
 }
