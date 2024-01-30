00383453a0bba17a8d4cbb14b72549781fc95857
dmay29 <david70may@gmail.com>
2024-01-29 23:53:06 -0500

    added Individual LED control

--- python/neopixel_i2c_driver.py
+++ python/neopixel_i2c_driver.py
@@ -1,5 +1,6 @@
 '''
 Comms signal plan
+-------------------------------------------------------------
 
 Composer Mode - PI initiates a strip to start counting down
 
@@ -19,6 +20,26 @@ Ex.
 Data bytes: 0b00000100, 0b00001000
 
 Would initiate a waterfall on keys 2 and 9
+
+--------------------------------------------------------------
+Individual LED Control - PI sets every LED individually, one byte per pixel, starting at pixel n
+
+Opcode = 0b1xxx
+
+Byte Makeup (bytes sorted in received order)
+
+|---------------------------|--------------------------------|
+|       Data Byte 0         |        Data Byte 1 -> n        |
+|---------------------------|--------------------------------|
+|  Bit 0  |     Bits 1-7    | Bits 0-2 | Bits 3-4 | Bits 5-7 |
+|---------------------------|--------------------------------|
+|    1    | First LED index |    Red   |   Green  |   Blue   |
+|---------------------------|--------------------------------|
+
+Ex.
+Data bytes: 0b10000000, 0b11100000, 0b00011000, 0b00000111,
+
+Would set the first 3 pixels to be red, green, blue
 
 
 '''
