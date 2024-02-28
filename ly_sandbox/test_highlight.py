5f0ab8276cf6c1ad6dfd0a295242ab30db09136f
Peyton Sullivan <sullivan.pey@northeastern.edu>
2024-02-27 19:05:23 -0500

    fix multi note replacement

--- ly_sandbox/test_highlight.py
+++ ly_sandbox/test_highlight.py
@@ -12,6 +12,7 @@ with open(FILE_NAME) as ly_file:
     note_lines = [line for line in note_lines if not ("\\" in line or "|" in line)]
     print(note_lines)
     total_note_count = 0
+    note_prev_location = {}
     for line in note_lines:
         notes = line.split(" ")
         notes = [note for note in notes if len(note) > 0]
@@ -23,7 +24,17 @@ with open(FILE_NAME) as ly_file:
                     i += 1
                     note += " " + notes[i]
             note += " "
-            ly_highlighted_note = ly_no_highlight.replace(note, "\staffHighlight \"lightsteelblue\"\n" + note + "\n" + "\stopStaffHighlight\n")
+            if note not in note_prev_location.keys():
+                note_prev_location[note] = 0
+            note_location = ly_no_highlight.find(note, note_prev_location[note])
+            print(f"note location {note_location}")
+            ly_no_highlight_first_part = ly_no_highlight[:note_location:]
+            ly_no_highlight_second_part = ly_no_highlight[note_location:note_location + len(note):]
+            ly_no_highlight_third_part = ly_no_highlight[note_location + len(note)::]
+            print(f"first {ly_no_highlight_first_part}")
+            print(f"second {ly_no_highlight_second_part}")
+            note_prev_location[note] = note_location
+            ly_highlighted_note = ly_no_highlight_first_part + ly_no_highlight_second_part.replace(note, "\staffHighlight \"lightblue\"\n\override NoteHead.color = #red\n" + note + "\n" + "\stopStaffHighlight\n\override NoteHead.color = #black\n") + ly_no_highlight_third_part
             with open("highlight_output/" + str(total_note_count) + FILE_NAME, "w") as output_ly_file:
                 output_ly_file.write(ly_highlighted_note)
             i += 1
